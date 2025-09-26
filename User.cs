91dfddf277158fc34711ea3a6b74e7223d46717d
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-09-26 16:18:22 +0200

    add method SetPassword with helper TryPassword
    To confirm password is the one you want

--- User.cs
+++ User.cs
@@ -1,15 +1,34 @@
+using System.Dynamic;
+
 namespace App;
 
 class User
 {
-    string Name;
-    string Email;
-    string _password;
+    public string? Name;
+    public string? Email;
+    string? _password;
 
     public User(string name, string email)
     {
         Name = name;
         Email = email;
+    }
+    public void SetPassword()
+    {
+        string TryPassword()
+        {
+            Console.Write("Input password: ");
+            string? attempt1 = Console.ReadLine();
+            Console.Write("Confirm password: ");
+            string? attempt2 = Console.ReadLine();
+            if (attempt1 != attempt2)
+            {
+                Utility.Error("Passwords not matching\nTry again..");
+                return TryPassword();
+            }
+            return attempt1;
+        }
+        TryPassword();
     }
     public bool TryLogin(string email, string password)
     {
