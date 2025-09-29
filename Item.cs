a3ba71c6619179c502f0eaa30ba807fdca854f38
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-09-29 19:21:35 +0200

    add Owner class name to method Info

--- Item.cs
+++ Item.cs
@@ -15,6 +15,6 @@ class Item
     }
     public string Info()
     {
-        return $"Name:\n{Name}\nDescription:\n{Description}";
+        return $"Name:\n-{Name}-\nDescription:\n'{Description}'\nOwner:\n-{Owner.Name}";
     }
 }
